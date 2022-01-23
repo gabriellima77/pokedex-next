3fb4554af5927a858483eec9535bab077ac7bc82
Gabriel Lima <gabrielpk1@live.com>
2022-01-22 23:13:08 -0300

    add navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,42 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Nav.module.css';
 
 const Nav = () => {
-  return <nav className={styles.nav}>
-    <ul>
-      <li>
-        <Link href="/bookmarks" passHref>
-          <i className="fas fa-heart"></i> Favoritos
-        </Link>
-        Favoritos
-      </li>
-    </ul>
-  </nav>;
+  const [hasSearchBar, setHasSearchBar] = useState(true);
+
+  const getSearchBar = () => {
+    return (
+      <div className={styles.searchContainer}>
+        <input
+          id="search"
+          className={styles.search}
+          type="text"
+          placeholder="Search"
+        />
+        <label htmlFor="search">
+          <i className="fas fa-search" />
+        </label>
+      </div>
+    );
+  };
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.above}>
+        <h1 className={styles.title}>PokeDex</h1>
+        <ul className={styles.list}>
+          <li>
+            <Link href="/bookmarks" passHref>
+              <i className="fas fa-heart" />
+            </Link>
+            <Link href="/bookmarks">Favoritos</Link>
+          </li>
+        </ul>
+      </div>
+      {getSearchBar()}
+    </nav>
+  );
 };
 
 export default Nav;
